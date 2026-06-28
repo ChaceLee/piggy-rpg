81bb605bd0a6f7d421f48508f146ff4fd409d83b
Chacelee <14492941+chacelee_90@user.noreply.gitee.com>
2026-06-28 18:22:48 +0800

    Add build output to root for GitHub Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   { path: '/', name: 'home', component: () => import('../views/WorldMap.vue') },
@@ -14,6 +14,6 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
